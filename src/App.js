432274dc5b839ffbcde146fa27edc447abec552c
Török László <toroklaszlo86@gmail.com>
2021-03-22 16:09:22 +0100

    megold

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import './App.css'
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import LoadingMask from './components/LoadingMask';
+import Hotel from './components/Hotel';
+import Subscription from './components/Subscription';
+
 
 const App = () => {
 
+  const [ isLoadingHotels, setIsLoadingHotels] = useState(false)
+  const [ hoteldata, setHotelData] = useState([])
+  const [showForm, setShownForm] = useState(false)
+
+  useEffect(() => {
+    setIsLoadingHotels(true)
+    fetch("api/hotels")
+      .then(res => res.json())
+      .then(data => setHotelData(data))
+      .catch(err => setHotelData(null))
+      .finally(() => setIsLoadingHotels(false))
+  }, [])
+
+
   return (
     <div className="App">
-      Todo...
+      {
+        isLoadingHotels && <LoadingMask />
+      }
+      {
+        hoteldata ? 
+        hoteldata.map(hotel => <Hotel key={hotel.name} name={hotel.name} city={hotel.city} stars={hotel.stars} />) : 
+        <p>Oooooopppssss, something happened</p>
+      }
+      {
+        showForm && <Subscription close={() => setShownForm(false)} />
+      }
     </div>
   )
 }
---
+++ src/components/LoadingMask.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+const LoadingMask = () => {
+
+    return (
+        <div>
+            Hey Hey Loading....
+        </div>
+    )
+}
+
+export default LoadingMask;
